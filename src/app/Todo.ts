9affe41c3a5bcbac402ef4f17ae2347d7e8bb28e
Purushottam Samlety <78149158+Purushottam18@users.noreply.github.com>
2022-01-25 10:35:22 +0530

    Initial Commit

---
+++ src/app/Todo.ts
@@ -0,0 +1,12 @@
+export class Todo{
+    constructor(){
+        this.sno=0;
+        this.title="";
+        this.des="";
+        this.active=true;
+    }
+    sno: number
+    title: string
+    des:string
+    active:boolean
+}
